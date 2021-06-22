cb7ce1d261693219470b439960b2b1094bd82191
Developer-Nasim <unique0coder@gmail.com>
2021-06-22 19:42:17 +0600

    up all

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+
+  // owlCarousel
+  $(".project_slider").owlCarousel({
+    loop: true,
+    margin: 20,
+    items: 1,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      767: {
+        items: 1
+      },
+      992: {
+        items: 1
+      }
+    }
+  });
+
+
+ 
+})(jQuery);
